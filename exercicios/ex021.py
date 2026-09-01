01dd5817587291dd366db1cce14e13cc290a8bce
JEBAS7 <jebas7@outlook.com>
2026-09-01 13:46:52 -0300

    Python 3 aula08 utilizando módulos exercícios

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,4 +1,11 @@
-import winsound
+# import winsound
 
 # O Windows vai ler e reproduzir o som de forma nativa instantaneamente
-winsound.PlaySound('ex021.wav', winsound.SND_FILENAME)
+# winsound.PlaySound('ex021.wav', winsound.SND_FILENAME)
+
+from playsound import playsound
+
+# Ela toca o áudio de forma nativa e segura
+playsound('ex021.mp3')
+
+
