da1d0a18ace922e803f74149497644ccee383d75
JEBAS7 <97810753+JEBAS7@users.noreply.github.com>
2026-09-14 04:07:55 -0300

    Python 3 aula10 condições parte 1 exercícios

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,7 +1,12 @@
 from random import randint
-
+from time import sleep
 cpu = randint(0, 5)
+print('-=' * 20)
+print('Vou pensar em um número entre 0 e 5.')
+print('-=' * 20)
 n = int(input('Escolha um número de 0 a 5: '))
+print('PROCESSANDO...')
+sleep(3)
 if cpu == n:
     print(f'Parabéns! você venceu o número escolhido foi {cpu} ')
 else:
