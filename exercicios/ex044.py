785cc584dcbfdbb33f6d9ef8e2ed95ab0c015fde
JEBAS7 <97810753+JEBAS7@users.noreply.github.com>
2026-09-16 21:52:37 -0300

    Python 3 aula12 condições aninhadas exercícios

--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -1,18 +1,23 @@
-preco = float(input('Digite o preço do produto: '))
+preco = float(input('Digite o preço do produto: R$'))
 print('Forma de pagamento\n'
       'Digite 1 para dinheiro ou cheque a vista\n'
       'Digite 2 para cartão a vista\n'
       'Digite 3 para 2x no cartão\n'
       'Digite 4 para 3x ou mais no cartão')
 pagamento = int(input('Como você vai pagar? '))
-if 1:
+if pagamento == 1:
     valor = preco - preco * 10 / 100
-    print(f'Com 10% de desconto a vista o preço é {valor} ')
-if 2:
+    print(f'Com 10% de desconto a vista o preço é R${valor:.2f} ')
+elif pagamento == 2:
     valor = preco - preco * 5 / 100
-    print(f'Com 5 de desconto a vista no cartão o preço é {valor}')
-if 3:
-    print(f'Com 2x no cartão o preço é {preco}')
-if 4:
+    print(f'Com 5% de desconto a vista no cartão o preço é R${valor:.2f}')
+elif pagamento == 3:
+    parcela = preco / 2
+    print(f'Com 2x no cartão o preço é R${preco:.2f} e cada parcela é R${parcela:.2f}')
+elif pagamento == 4:
+    total_parcelas = int(input('Em quantas parcelas'))
     valor = preco + preco * 20 / 100
-    print(f'Com 3x ou mais no cartão tem 20% de juros e o preço é {valor}')
+    parcela = valor / total_parcelas
+    print(f'Com {total_parcelas}x no cartão, tem 20% de juros. O preço é R${valor:.2f} em {total_parcelas}x de R${parcela:.2f}')
+else:
+    print(f'Erro! você digitou {pagamento}, não exite essa forma de pagamento.')
---
+++ exercicios/ex045.py
@@ -0,0 +1,30 @@
+from random import choice
+from time import sleep
+
+print('\033[1;33m-=\033[m' * 30)
+print('{:^70}'.format('\033[1;31m JOKENPÔ \033[m'))
+print('\033[1;33m-=\033[m' * 30)
+jogador = str(input('Escolha PEDRA, PAPEL, TESOURA: ')).strip().upper()
+cpu = choice(['PEDRA', 'PAPEL', 'TESOURA'])
+print('\033[1;33mJO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ\033[m')
+sleep(1)
+if cpu == jogador:
+    print(f'Computador escolheu {cpu}, Jogador escolheu {jogador}')
+    print('Jogo empatou!')
+elif cpu == 'PEDRA' and jogador == 'PAPEL':
+    print(f'Computador escolheu {cpu}. Jogador escolheu {jogador}')
+    print('\033[1;32mJogador venceu!\033[m')
+elif cpu == 'PAPEL' and jogador == 'TESOURA':
+    print(f'Computador escolheu {cpu}. Jogador escolheu {jogador}')
+    print('\033[1;32mJogador venceu!\033[m')
+elif cpu == 'TESOURA' and jogador == 'PEDRA':
+    print(f'Computador escolheu {cpu}. Jogador escolheu {jogador}')
+    print('\033[1;32mJogador venceu!\033[m')
+else:
+    print(f'Computador escolheu {cpu}. Jogador escolheu {jogador}')
+    print('\033[1;31mJogador perdeu!\033[m')
+
