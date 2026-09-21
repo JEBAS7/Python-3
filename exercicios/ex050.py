02d467f52ae9ecd9798233ccc21a8dd1004a397d
JEBAS7 <97810753+JEBAS7@users.noreply.github.com>
2026-09-21 06:51:07 -0300

    Python 3 aula13 estrutura de repetição FOR exercícios

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,7 +1,7 @@
 soma = 0
 cont = 0
 for c in range(1, 7):
-    n = int(input('Digite um número: '))
+    n = int(input(f'Digite o {c}º número: '))
     if n % 2 == 0:
         soma += n
         cont += 1
